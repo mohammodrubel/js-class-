86289b36536e211ac8af7a80123aeac7976a80cc
mohammodrubel <mohammodrubel>
2022-04-14 22:51:58 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+class car {
+    constructor(name,year){
+        this.name=name;
+        this.year=year
+    }
+}
+
+const carOne = new car ("toyota",2007) // create actual object
+const carTwo = new car ("BMW",2004) // create actual object
+const carThree = new car ("Mercedes",2002) // create actual object
+const carFore = new car ("Lamborghini",2006) // create actual object
+
+
+
+console.log(carOne)
+console.log(carTwo)
+console.log(carThree)
+console.log(carFore)
